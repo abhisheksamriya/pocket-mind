5156dd3db9e4aa7e7f431d93d97099e6d21934e9
abhishek samriya <abhisheksamriya2004@gmail.com>
2025-07-01 08:41:52 +0530

    Update index.ts fixing pushing issues

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,12 +15,7 @@ const databaseUrl = process.env.DATABASE_URL || "";
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://192.168.1.10:5173", // Tumhara frontend ka exact IP
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/content", contentRoutes);
